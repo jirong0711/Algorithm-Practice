dffe30814b1e181315ad071ddb1f7a23d2776afa
Jirong <qkffpsxkdlwm@naver.com>
2022-03-31 16:37:54 +0900

    codeup-python

---
+++ python/codeup_1084.py
@@ -0,0 +1,10 @@
+import sys
+
+r,g,b = map(int,sys.stdin.readline().split())
+sum = 0
+for i in range(r):
+    for j in range(g):
+        for k in range(b):
+            print(i,j,k)
+            sum += 1
+print(sum)
